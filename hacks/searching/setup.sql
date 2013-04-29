f3f4edd64cf11d92145eaabafd0ae5b13ae670e5
Tim Coates <tim.coates@gmail.com>
2013-04-29 07:48:31 +0100

    Created setup.sql file, for database creation. Also minor changes to php file which interacts with database.

---
+++ hacks/searching/setup.sql
@@ -0,0 +1,29 @@
+-- Table to hold the people searching...
+CREATE  TABLE `blc_db`.`searchprofile` (
+  `id` INT NOT NULL ,           -- This is the person's ID number, used in all subsequent transactions.
+  `forename` VARCHAR(45) NULL , -- Self explanatory.
+  `surname` VARCHAR(45) NULL ,  -- Self explanatory.
+  `mobile` VARCHAR(45) NULL ,   -- We will record their mobile number.
+  PRIMARY KEY (`id`) );         -- ID is the primary key in this table.
+
+
+-- Table to hold people's locations...
+CREATE TABLE `locations` (
+  `id` int(11) DEFAULT NULL,        -- The person's ID number from searchprofile.id.
+  `lat` decimal(10,0) DEFAULT NULL, -- Last recorded lattitude.
+  `long` decimal(10,0) DEFAULT NULL,-- Last recorded logitude.
+  `recorded` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,  -- Timestamp of when this record was recorded, to allow purging etc.
+  PRIMARY KEY (`recorded`),
+  KEY `byuser` (`id`)
+)
+
+-- Table to hold the text of any chats...
+  CREATE  TABLE `blc_db`.`chat` (
+  `id` INT NOT NULL ,           -- Simple autoincrmeenting id for each record.
+  `chat` VARCHAR(160) NULL ,    -- The text of the message - limit to 160, so it has the same 'feel' as SMS
+  `userid` INT NULL ,           -- User's ID from searchprofile.id above.
+  `lat` DECIMAL NULL ,          -- Lattitude of user at the point they sent in this chat, or in case of 'outbound' messages, last recorded location of user.
+  `long` DECIMAL NULL ,         -- Longitude of user (same as for lattitude applies).
+  PRIMARY KEY (`id`) );         -- Primary key is the rowid.
+-- now add an index to be able to get chats for a specific user...
+ALTER TABLE `blc_db`.`chat` ADD INDEX `byuser` (`userid` ASC, `id` ASC) ;
--- hacks/searching/submit_location.php
+++ hacks/searching/submit_location.php
@@ -21,39 +21,57 @@ mysql_select_db("$db_name")or die("cannot select DB");
 // 
 $action=$_POST[action];
 
+
 // They're registering as a new searcher - add them into the database, and return an id
 if($action=='register'){
 	$forename=$_POST['forename'];
 	$surname=$_POST['surname'];
 	$mobile=$_POST['mobile'];
 
-	$sql="INSERT INTO searchprofile(forename, surname, mobile)VALUES('$forename', '$surname', '$mobile')";
-	$id = mysql_insert_id();
+	$sql="INSERT INTO searchprofile(forename, surname, mobile)VALUES('$forename', '$surname', '$mobile');";
+	$output = mysql_insert_id();
 }
 
-// 
+// They've just sent their latest location - save theirs, then return a collection of other people
 if($action=='locate') {
 	$lat=$_POST['lat'];
 	$long=$_POST['long'];
 	$id=$_POST['id'];
 	
-	$sql="INSERT INTO locations(lat, long, userid)VALUES('$lat', '$long', '$id')";
+	// To simplify the INSERT or UPDATE complexity, we'll simply do a delete, immediately followed by an insert.
+	$sql="DELETE FROM locations WHERE userid = $id;";
+	$result=mysql_query($sql);
+	
+	$sql="INSERT INTO locations(lat, long, userid)VALUES($lat, $long, $id);";
+	$result=mysql_query($sql);
+	
+	// If successfully insert data into database, we now get all the locations (except our own)
+	if($result){
+		$sth = mysql_query("SELECT lat, long FROM locations WHERE userid <> $id;");
+		$rows = array();
+		while($r = mysql_fetch_assoc($sth)) {
+			$rows[] = $r;
+		}
+		$output = json_encode($rows);
+	}
 }
 
+// They've just sent in a 'chat' message. Store it into the database - we'll worry about what to do with it from there.
 if($action=='chat'){
 	$chat=$_POST['chat']; // Text of the chat
 	$lat=$_POST['lat'];
 	$long=$_POST['long'];
 	$id=$_POST['id'];    // User's ID number
 
-	$sql="INSERT INTO chat(chat, userid, lat, long )VALUES('$chat', '$id', '$lat', '$long')";
+	$sql="INSERT INTO chat(chat, userid, lat, long )VALUES('$chat', $id, $lat, $long);";
+	$output = "Success"
 }
 
 $result=mysql_query($sql);
 
 // if successfully insert data into database, displays message "Successful". 
 if($result){
-	echo $id;
+	echo $output;
 }
 
 mysql_close();
