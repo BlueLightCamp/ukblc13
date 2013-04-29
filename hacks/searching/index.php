795fc76311a87ef9cce512479dd4ce02cf69d8bd
Tim Coates <tim.coates@gmail.com>
2013-04-29 19:16:21 +0100

    Added PopBox code to start on the pop-up message box to support chatting.
    This works on index.html (a really basic proof of concept copied from the how-to info in README.md), but doesnt in index.php. I THINK think is simply because there is no screen left for it, but not certain.

--- hacks/searching/index.php
+++ hacks/searching/index.php
@@ -13,19 +13,44 @@
 		<link rel="stylesheet" href="http://cdn.leafletjs.com/leaflet-0.5/leaflet.ie.css" />
 	<![endif]-->
     <link href="css/custom.css" rel="stylesheet" media="screen">
+    <link rel='stylesheet' href='css/popbox.css' type='text/css' media='screen' charset='utf-8'>
   </head>
   <body>
   	<div id="wrapper">
   		<div id="loco"></div>
   		<div id="map"></div>
-  		<!--div id="video">
-  			<video autoplay></video>
-  		</div-->
+
+		
+				<div class='popbox'>
+		  <a class='open' href='#'><img src='plus.png' style='width:14px;position:relative;'> Click Here!</a>
+		  <div class='collapse'>
+		    <div class='box'>
+		      <div class='arrow'></div>
+		      <div class='arrow-border'></div>
+		        <form action="submit_location.php" method="post" id="subForm">
+			  <p><small>Please type your message to the Searcher</small></p>
+			  <div class="input">
+			    <input type="text" name="searcherid" id="idnumber" placeholder="Searcher ID will be in a hidden field here" />
+			  </div>
+			  <div class="input">
+			    <textarea name="chat" id="Message" placeholder="Comments"></textarea>
+			  </div>
+			  <input type="submit" value="Send" /> <a href="#" class="close">Cancel</a>
+			</form>
+		      </div>
+		    </div>
+		  </div>
+		
+		
+		
+		
+		
 	</div>
 	<script src="http://code.jquery.com/jquery.js"></script>
 	<script src="js/bootstrap.min.js"></script>
 	<script src="http://cdn.leafletjs.com/leaflet-0.5/leaflet.js"></script>
 	<!--script type="text/javascript" src="http://openspace.ordnancesurvey.co.uk/osmapapi/openspace.js?key=DB6BA21C2386B465E0405F0ACA6034A7"></script-->
 	<script src="js/custom.js"></script>
+	<script src="js/popbox.js"></script>
   </body>
 </html>
