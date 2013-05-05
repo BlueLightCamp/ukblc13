740a44d12de3ddfbc780698567df0f324809cda4
Tim Coates <tim.coates@gmail.com>
2013-05-05 22:33:42 +0100

    Backend script should now handle all interactions. Offers JSON collection of it's available interactions.

--- hacks/searching/setup.sql
+++ hacks/searching/setup.sql
@@ -1,6 +1,14 @@
+-- Table to hold the searches...
+CREATE  TABLE `blc_db`.`searchcase` (
+  `id` int(10) unsigned auto_increment PRIMARY KEY not null, -- This is the person's ID number, used in all subsequent transactions.
+  `name` VARCHAR(45) NULL ,                                  -- Self explanatory.
+  `coordinator` INT NULL ,                                   -- The Userid of the person acting as co-ordinator
+  `description` TEXT);                                       -- Self explanatory.
+
 -- Table to hold the people searching...
 CREATE  TABLE `blc_db`.`searchprofile` (
   `id` int(10) unsigned auto_increment PRIMARY KEY not null, -- This is the person's ID number, used in all subsequent transactions.
+  `searchcase_id` INT ,                                      -- The search this searcher is searching for
   `forename` VARCHAR(45) NULL ,                              -- Self explanatory.
   `surname` VARCHAR(45) NULL ,                               -- Self explanatory.
   `mobile` VARCHAR(45) NULL);                                -- Mobile phone number.
@@ -8,22 +16,23 @@ CREATE  TABLE `blc_db`.`searchprofile` (
 
 -- Table to hold people's locations...
 CREATE TABLE `locations` (
-  `id` int(11) DEFAULT NULL,        -- The person's ID number from searchprofile.id.
+  `searchprofile_id` int(11) DEFAULT NULL,        -- The person's ID number from searchprofile.id.
   `lat` decimal(10,0) DEFAULT NULL, -- Last recorded lattitude.
   `lon` decimal(10,0) DEFAULT NULL,-- Last recorded logitude.
   `recorded` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,  -- Timestamp of when this record was recorded, to allow purging etc.
   PRIMARY KEY (`recorded`),
-  KEY `byuser` (`id`)
-)
+  KEY `byuser` (`searchprofile_id`)
+);
 
 -- Table to hold the text of any chats...
   CREATE  TABLE `blc_db`.`chat` (
   `id` INT NOT NULL ,           -- Simple autoincrmeenting id for each record.
   `chat` VARCHAR(160) NULL ,    -- The text of the message - limit to 160, so it has the same 'feel' as SMS
-  `userid` INT NULL ,           -- User's ID from searchprofile.id above. Usually same as fromuser field, apart from replies, and broadcasts.
+  `searchprofile_id` INT NULL ,           -- User's ID from searchprofile.id above. Usually same as fromuser field, apart from replies, and broadcasts.
   `lat` DECIMAL NULL ,          -- Lattitude of user at the point they sent in this chat, or in case of 'outbound' messages, last recorded location of user.
   `lon` DECIMAL NULL ,          -- Longitude of user (same as for lattitude applies).
-  `fromuser` INT NULL,          -- The user creating this statement (Coordinator = -1). See userid above - needed to target replies and enable broadcasts.
+  `fromuser_id` INT NULL,          -- The user creating this statement (Coordinator = -1). See userid above - needed to target replies and enable broadcasts.
   PRIMARY KEY (`id`) );         -- Primary key is the rowid.
 -- now add an index to be able to get chats for a specific user...
-ALTER TABLE `blc_db`.`chat` ADD INDEX `byuser` (`userid` ASC, `id` ASC) ;
+ALTER TABLE `blc_db`.`chat` ADD INDEX `byuser` (`searchprofile_id` ASC, `id` ASC);
+
--- hacks/searching/submit_location.php
+++ hacks/searching/submit_location.php
@@ -12,35 +12,52 @@ mysql_connect("$host", "$username", "$password")or die("cannot connect");
 mysql_select_db("$db_name")or die("cannot select DB");
 $sql = '';
 
-// Action can be one of:
-// register
-// locate
-// chat
-// locations
-// reply
-$action=$_GET['action'];
+$action=$_GET["action"];
 
 
 // Here we respond to a plain old GET request, supplying the API description as a JSON response.
 if(!isset($action))
 {
 	$options = [
-		"register" => "Register a new searcher. Requires: forename | surname | mobile Returns: id",
-		"locate" => "Record a searcher's location. Requires: lat | long | id (from register) Returns: JSON array of other searchers or ERROR.",
-		"chat" => "Submit a chat message back to the coordinator. Requires: chat | lat | long | id (from register) Returns: OK or ERROR",
-		"locations" => "List all searcher locations. Requires: [Nothing]. Returns: JSON array of all searchers or ERROR.",
-		"reply" => "Reply to a chat from a searcher. Requires: chat | id (from register) Returns: OK or ERROR",
+		"create" => "Create a new search. Requires: name | coordinator | description - Returns: search_id or ERROR",
+		"register" => "Register a new searcher. Requires: forename | surname | mobile | search_id - Returns: id or ERROR",
+		"locate" => "Record a searcher's location. Requires: lat | long | id (from register) - Returns: JSON array of other searchers or ERROR.",
+		"chat" => "Submit a chat message back to the coordinator. Requires: chat | lat | long | id (from register) - Returns: OK or ERROR",
+		"locations" => "List all searcher locations. Requires: coordinator -  Returns: JSON array of all searchers or ERROR.",
+		"reply" => "Reply to a chat from a searcher. Requires: chat | id (from register) - Returns: OK or ERROR",
+		"broadcast" => "Broadcasts to all searchers in the search this co-ordinator is the co-ordinator of. - Returns: OK or ERROR",
 	];
 	die(json_encode($options));
 }
 
-// They're registering as a new searcher - add them into the database, and return an id
-if($action=='register'){
-	$forename=$_GET['forename'];
-	$surname=$_GET['surname'];
-	$mobile=$_GET['mobile'];
+// They're creating a new search - add it into the database, and return it's id
+if($action=='create') {
 
-	$sql="INSERT INTO searchprofile(forename, surname, mobile)VALUES('$forename', '$surname', '$mobile');";
+	$name        = $_GET['name'];
+	$coordinator = $_GET['coordinator'];
+	$description = $_GET['description'];
+	
+	$sql="INSERT INTO blc_db.searchcase(name, coordinator, description) VALUES ('" . $name . "', " . $coordinator . ", '" . $description . "');";
+	
+	$result=mysql_query($sql);
+	
+	$response = [
+		"id" => mysql_insert_id(),
+	];
+	mysql_close();
+	die(json_encode($response));
+}
+
+// They're registering as a new searcher - add them into the database, and return an id
+if($action=='register') {
+
+	$forename = $_GET["forename"];
+	$surname  = $_GET["surname"];
+	$mobile   = $_GET["mobile"];
+	$searchid = $_GET['search_id'];
+	
+	$sql="INSERT INTO blc_db.searchprofile(searchcase_id, forename, surname, mobile) VALUES (" . $searchid . ", '" . $forename . "', '" . $surname . "', '" . $mobile . "');";
+	
 	$result=mysql_query($sql);
 	
 	$response = [
@@ -52,21 +69,23 @@ if($action=='register'){
 
 // They've just sent their latest location - save theirs, then return a collection of other people
 if($action=='locate') {
-	$lat=$_GET['lat'];
-	$long=$_GET['long'];
-	$id=$_GET['id'];
+
+	$id   = $_GET['id'];
+	$lat  = $_GET['lat'];
+	$long = $_GET['long'];
 	
 	// To simplify the INSERT or UPDATE complexity, we'll simply do a delete, immediately followed by an insert.
-	$sql="DELETE FROM locations WHERE userid = $id;";
+	$sql="DELETE FROM blc_db.locations WHERE userid = $id;";
 	$result=mysql_query($sql);
 
-	$sql="INSERT INTO blc_db.locations(userid, lat, lon) VALUES($id, $lat, $long);";
+	$sql="INSERT INTO blc_db.locations(searchprofile_id, lat, lon) VALUES(" . $id . ", " . $lat . ", " . $long . ");";
 	
+	//die($sql);
 	$result=mysql_query($sql);
 	
 	// If successfully insert data into database, we now get all the locations (except our own)
 	if($result){
-		$sth = mysql_query("SELECT lat, long FROM locations WHERE userid <> $id;");
+		$sth = mysql_query("SELECT lat, lon FROM locations WHERE searchprofile_id <> $id;");
 		$rows = array();
 		while($r = mysql_fetch_assoc($sth)) {
 			$rows[] = $r;
@@ -78,16 +97,16 @@ if($action=='locate') {
 
 // They've just sent in a 'chat' message. Store it into the database - we'll worry about what to do with it from there.
 if($action=='chat'){
-	$chat=$_GET['chat']; // Text of the chat
-	$lat=$_GET['lat'];
-	$long=$_GET['long'];
-	$id=$_GET['id'];    // User's ID number
+	
+	$chat = $_GET['chat'];  // Text of the chat
+	$id   = $_GET['id'];    // User's ID number
+	$lat  = $_GET['lat'];   // Current location is recorded too
+	$long = $_GET['long'];
 
-	$sql="INSERT INTO chat(chat, userid, lat, long) VALUES ('$chat', $id, $lat, $long);";
+	$sql="INSERT INTO chat(chat, searchprofile_id, lat, lon) VALUES ('" . $chat . "', " . $id . ", " . $lat . ", " . $long . ");";
 	$result=mysql_query($sql);
 	mysql_close();
 
-	// if successfully insert data into database, displays message "Successful". 
 	if($result){
 		$response = [
 			"result" => "OK",
@@ -97,16 +116,18 @@ if($action=='chat'){
 			"result" => "ERROR",
 		];		
 	}
-
 	die(json_encode($response));
 }
 
 // This to get all searchers' locations...
 if($action=='locations') {
 
-// First we should clear out anyone who hasn't updated their location in the last 5 minutes?
+// First we should clear out anyone who hasn't updated their location in the last 5 ?? minutes...
+	$sql = "DELETE FROM locations WHERE TIMESTAMPDIFF(MINUTE, recorded, CURRENT_TIMESTAMP) > 5;";
+	$sth = mysql_query($sql);
+	$coordinator = $_GET['coordinator'];
 
-	$sql = "SELECT lat, long FROM locations;";
+	$sql = "SELECT locations.lat, locations.lon, sp1.id FROM searchprofile sp1 JOIN searchcase ON sp1.searchcase_id = searchcase.id JOIN locations ON sp1.id = locations.searchprofile_id WHERE searchcase.coordinator = " . $coordinator. ";";
 
 	$sth = mysql_query($sql);
 	$rows = array();
@@ -118,17 +139,17 @@ if($action=='locations') {
 }
 
 
-// This to get all searchers' locations...
+// This to send a chat message from co-ordinator to a searcher...
 if($action=='reply') {
 
 	$chat=$_GET['chat'];	// Text of the chat
 	$id=$_GET['id'];	// User's ID number
+	$coord=$_GET['coord'];	// User ID of the co-ordinator
 	$sql = "INSERT INTO blc_db.chat(chat, userid, fromuser) VALUES($chat, $id, -1);";
 	$sth = mysql_query($sql);
 	mysql_close();
 	
 	
-	// if successfully insert data into database, displays message "Successful". 
 	if($sth){
 		$response = [
 			"result" => "OK",
@@ -138,7 +159,6 @@ if($action=='reply') {
 			"result" => "ERROR",
 		];		
 	}
-
 	die(json_encode($response));
 }
 
@@ -150,8 +170,6 @@ if($action=='broadcast'){
 	$sth = mysql_query($sql);
 	mysql_close();
 	
-	
-	// if successfully insert data into database, displays message "Successful". 
 	if($sth){
 		$response = [
 			"result" => "OK",
@@ -161,7 +179,6 @@ if($action=='broadcast'){
 			"result" => "ERROR",
 		];		
 	}
-
 	die(json_encode($response));
 }
 ?>
